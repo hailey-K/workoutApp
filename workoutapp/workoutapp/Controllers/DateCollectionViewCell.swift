f0bc5f34b169191855d16badf1508c7ea4493f54
BSH <tmdghksi86@gmail.com>
2018-04-13 21:19:35 -0400

    Added new files

---
+++ workoutapp/workoutapp/Controllers/DateCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateCollectionViewCell.swift
+//  workoutapp
+//
+//  Created by Harmandeep Kaur on 2018-04-07.
+//  Copyright © 2018 hch_enterprise. All rights reserved.
+//
+
+import UIKit
+
+class DateCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var DateLabel: UILabel!
+}
